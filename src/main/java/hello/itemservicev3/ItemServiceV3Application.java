eb90031b7a74135740074978cc69326c518fdd05
geunhokinn <gghh1220@gmail.com>
2024-05-03 18:50:12 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/hello/itemservicev3/ItemServiceV3Application.java
@@ -0,0 +1,13 @@
+package hello.itemservicev3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/hello/itemservicev3/ItemServiceV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.itemservicev3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
